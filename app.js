9f463258d8f831b16c9db2e896b2a6dfb0d12952
teng14 <278697698@qq.com>
2016-03-07 07:55:00 +0800

    优化结构和代码

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-var express    = require('express');
+/*
+ * 
+ */
 
+var express    = require('express');
 var app        = express();
 var path       = require('path');
-
 var bodyParser = require('body-parser');
 var multer     = require('multer'); 
 
