e192f1ef5089e0778ac7fdae4b784a5d231aab7a
mateng <278697698@qq.com>
2016-04-18 18:10:42 +0800

    添加 爬取远程资源代码

--- routes/handle.js
+++ routes/handle.js
@@ -1,4 +1,6 @@
 var xlsx       = require('node-xlsx');
+var cheerio	= require('cheerio');
+var server = require("./getResourse");
 
 module.exports = function(app){
 	app.post('/handle', function(req, res){
@@ -11,8 +13,34 @@ module.exports = function(app){
 			objExcel = xlsx.parse(filePath);
 			//获取Excel 第一张表
 			//返回JSON数据
+			var getResourseArray=[];
+			for(var i=0; i< objExcel[0].data.length; i++){
+				var url = objExcel[0].data[i][objExcel[0].data[i].length-1]
+				var j = i;
+				server.download(url, function(data) {
+				  if (data) {
+				    var ___ = cheerio.load(data);
+					var goodsId = ___('#GoodsId').val();
+					var imgUrl = ___('.bigimg').attr('src');
+					console.log(goodsId)
+					//objExcel[0].data[0].push('34321');
+					// objExcel[0].data[1].imgurl = imgUrl;
+				    console.log("done");
+				  } else {
+				      console.log("error");
+				  } 
+				});
+			}
+			// objExcel[0].data[0].goodsid = 111
+			//console.log(objExcel[0].data)
 			res.json(objExcel[0].data);
 			//res.send(200)
 		}
-	})
+	});
+
+
+	function getImgUrl(str){
+		var index = str.indexOf('_300');
+		return str.substring(0,index);
+	}
 }
